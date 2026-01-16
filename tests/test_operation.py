d380a43bad7ac81d9841f0fa68f96dca661ebb1c
Abhishek Patel <abhipatel7918@gmail.com>
2026-01-16 11:56:19 +0530

    test 1

--- tests/test_operation.py
+++ tests/test_operation.py
@@ -8,3 +8,12 @@ def test_sub():
         assert sub(2,3) == -1
         assert sub(5,5) == 0
         assert sub(10,3) == 7
+        
+def test_mul():
+        assert mul(2,3) == 6
+        assert mul(5,5) == 25
+        assert mul(10,3) == 30
+
+def test_div():
+    assert add(3,3) == 1
+    assert add(10,5) == 2
